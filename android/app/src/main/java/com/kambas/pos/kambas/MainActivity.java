f15d27c680a5f0a3af3ca2a54e500d6a8a9545ac
Hope Labarda <mhopelabarda@gmail.com>
2024-06-14 12:52:28 +0800

    * updated font content in receipt

--- android/app/src/main/java/com/kambas/pos/kambas/MainActivity.java
+++ android/app/src/main/java/com/kambas/pos/kambas/MainActivity.java
@@ -279,13 +279,13 @@ public class MainActivity extends FlutterActivity {
                 mIPosPrinterService.printSpecifiedTypeText("Ganhas Possíveis - " + priceAmount + " kza", "ST", 24, callback);
 
                 mIPosPrinterService.printBlankLines(1, 16, callback);
-                mIPosPrinterService.printSpecifiedTypeText("--------------------------", "ST", 16, callback);
+                mIPosPrinterService.printSpecifiedTypeText("--------------------------", "ST", 24, callback);
                 mIPosPrinterService.printBlankLines(1, 16, callback);
-                mIPosPrinterService.printSpecifiedTypeText("Válido 7 dias após o ganho", "ST", 24, callback);
+                mIPosPrinterService.printSpecifiedTypeText("Válido 7 dias após o ganho", "ST", 32, callback);
                 mIPosPrinterService.printBlankLines(1, 16, callback);
-                mIPosPrinterService.printSpecifiedTypeText("KAMBAS BET", "ST", 24, callback);
+                mIPosPrinterService.printSpecifiedTypeText("KAMBAS BET", "ST", 32, callback);
                 mIPosPrinterService.printBlankLines(1, 16, callback);
-                mIPosPrinterService.printSpecifiedTypeText("Lei 15/23, Prêmios > 200 000 kz o excedente -> taxa 20%", "ST", 16, callback);
+                mIPosPrinterService.printSpecifiedTypeText("Lei 15/23, Prêmios > 200 000 kz o excedente -> taxa 20%", "ST", 32, callback);
                 mIPosPrinterService.printBlankLines(1, 16, callback);
                 mIPosPrinterService.printSpecifiedTypeText("Contacto kambasdeangola@gmail.com", "ST", 24, callback);
 
